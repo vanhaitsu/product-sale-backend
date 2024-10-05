b953ad856c50dc1d568183375cd925a702734eec
Ngoc Tuan <tuannnse170112@fpt.edu.vn>
2024-10-05 21:00:47 +0700

    fix: update template for send email

--- Services/Services/AccountService.cs
+++ Services/Services/AccountService.cs
@@ -88,9 +88,100 @@ namespace Services.Services
         private async Task SendVerificationEmail(Account account)
         {
             await _emailService.SendEmailAsync(account.Email!, "Verify your email",
-                $"Your verification code is {account.VerificationCode}. The code will expire in 15 minutes.", true);
+                GenerateEmailBody(account.FirstName, account.VerificationCode), true);
         }
+        private string GenerateEmailBody(string fullName, string otp)
+        {
+            return $@"
+   <body style=""display: flex; justify-content: center; align-items: center"">
+    <div>
+      <div
+        style=""
+          color: #536e88;
+          width: fit-content;
+          box-shadow: 0 2px 8px rgba(8, 120, 211, 0.2);
+          padding: 10px;
+          border-radius: 5px;
+        ""
+      >
+        <div
+          style=""
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 10px;
+            margin-top: 0px;
+            background-color: #3498db;
+            font-size: 0.875rem;
+            font-weight: bold;
+            color: #ffffff;
+          ""
+        ></div>
 
+        <h1 style=""text-align: center; color: #3498db"">
+          Welcome to
+          <span style=""color: #f99f41"">our website!</span>
+        </h1>
+
+        <div style=""text-align: center"">
+          <img
+            src=""https://img.freepik.com/free-vector/students-bus-transportation_24877-83765.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1715040000&semt=ais""
+            alt=""logo""
+            width=""70""
+          />
+        </div>
+
+        <p style=""text-align: center; font-weight: bold; margin-top: 0"">
+          <span style=""color: #f99f41"">THE PRODUCT </span
+          ><span style=""color: #3498db"">SALE</span>
+        </p>
+
+        <div
+          style=""
+            width: fit-content;
+            margin: auto;
+            box-shadow: 0 2px 8px rgba(8, 120, 211, 0.2);
+            padding-top: 10px;
+            border-radius: 10px;
+          ""
+        >
+          <p>
+            Xin chào,
+            <span style=""font-weight: bold; color: #0d1226"">{fullName}</span>
+          </p>
+          <p>
+            <span style=""font-weight: bold"">THE PRODUCT SALE </span>would like to inform you that your account has been successfully registered.<span></span>
+          </p>
+          <p>
+            <span>Your OTP code is: </span
+            ><span style=""color: #0d1226; font-weight: bold"">{otp}</span>
+          </p>
+          <p>The code will expire in 15 minutes !</p>
+          <p>Thank you very much for using our platform !</p>
+          <p>Pleasure,</p>
+          <p style=""font-weight: 700; color: #0d1226"">THE PRODUCT SALE</p>
+        </div>
+
+        <div
+          style=""
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 40px;
+            background-color: #3498db;
+            font-size: 0.875rem;
+            font-weight: bold;
+            color: #ffffff;
+          ""
+        >
+          © 2024 | Copyright belongs to THE PRODUCT SALE.
+        </div>
+      </div>
+    </div>
+  </body>
+
+    ";
+        }
         public async Task<ResponseDataModel<TokenModel>> Login(AccountLoginModel accountLoginModel)
         {
             var user = await _userManager.FindByNameAsync(accountLoginModel.Email);
